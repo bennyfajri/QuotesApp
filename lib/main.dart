077330f5b236ec4d9e4801b5f4ad9543fb9b4dce
Muhammad Beni Fajri <m.b.fajrii@gmail.com>
2024-06-19 13:52:17 +0700

    add redirecton

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:declarative_navigation/routes/page_manager.dart';
+import 'package:declarative_navigation/db/auth_repository.dart';
+import 'package:declarative_navigation/provider/auth_provider.dart';
 import 'package:declarative_navigation/routes/router_delegate.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -16,17 +17,22 @@ class QuotesApp extends StatefulWidget {
 
 class _QuotesAppState extends State<QuotesApp> {
   late MyRouterDelegate myRouterDelegate;
+  late AuthProvider authProvider;
 
   @override
   void initState() {
     super.initState();
-    myRouterDelegate = MyRouterDelegate();
+    final authRepository = AuthRepository();
+
+    authProvider = AuthProvider(authRepository: authRepository);
+
+    myRouterDelegate = MyRouterDelegate(authRepository: authRepository);
   }
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => PageManager(),
+      create: (context) => authProvider,
       child: MaterialApp(
         title: 'Quotes App',
         home: Router(
--- lib/routes/router_delegate.dart
+++ lib/routes/router_delegate.dart
@@ -1,64 +1,58 @@
+import 'package:declarative_navigation/db/auth_repository.dart';
 import 'package:declarative_navigation/screen/form_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../model/quote.dart';
+import '../screen/login_screen.dart';
 import '../screen/quote_detail_screen.dart';
 import '../screen/quotes_list_screen.dart';
+import '../screen/register_screen.dart';
+import '../screen/splash_screen.dart';
 
 class MyRouterDelegate extends RouterDelegate
     with ChangeNotifier, PopNavigatorRouterDelegateMixin {
-  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
+  final GlobalKey<NavigatorState> _navigatorKey;
+  final AuthRepository authRepository;
+
+  MyRouterDelegate({
+    required this.authRepository,
+  }) : _navigatorKey = GlobalKey<NavigatorState>() {
+    _init();
+  }
+
+  _init() async {
+    isLoggedIn = await authRepository.isLoggedIn();
+    notifyListeners();
+  }
 
   @override
   GlobalKey<NavigatorState>? get navigatorKey => _navigatorKey;
 
   String? selectedQuote;
   bool isForm = false;
+  List<Page> historyStack = [];
+  bool? isLoggedIn;
+  bool isRegister = false;
 
   @override
   Widget build(BuildContext context) {
+    if (isLoggedIn == null) {
+      historyStack = _splashStack;
+    } else if (isLoggedIn == true) {
+      historyStack = _loggedInStack;
+    }  else {
+      historyStack = _loggedOutStack;
+    }
     return Navigator(
         key: navigatorKey,
-        pages: [
-          MaterialPage(
-            key: const ValueKey("QuotesListPage"),
-            child: QuotesListScreen(
-              quotes: quotes,
-              onTapped: (quoteId) {
-                selectedQuote = quoteId;
-                notifyListeners();
-              },
-
-              toFormScreen: (){
-                isForm = true;
-                notifyListeners();
-              },
-            ),
-          ),
-          if (selectedQuote != null)
-            MaterialPage(
-              key: ValueKey("QuotesDetailPage-$selectedQuote"),
-              child: QuoteDetailsScreen(
-                quoteId: selectedQuote!,
-              ),
-            ),
-          if (isForm)
-            MaterialPage(
-              key: const ValueKey("FormScreen"),
-              child: FormScreen(
-                onSend: () {
-                  isForm = false;
-                  notifyListeners();
-                },
-              ),
-            )
-        ],
+        pages: historyStack,
         onPopPage: (route, result) {
           final didPop = route.didPop(result);
           if (!didPop) {
             return false;
           }
 
+          isRegister = false;
           selectedQuote = null;
           isForm = false;
           notifyListeners();
@@ -71,4 +65,79 @@ class MyRouterDelegate extends RouterDelegate
   Future<void> setNewRoutePath(configuration) {
     throw UnimplementedError();
   }
+
+  List<Page> get _splashStack => const [
+        MaterialPage(
+          key: ValueKey("SplashPage"),
+          child: SplashScreen(),
+        ),
+      ];
+
+  List<Page> get _loggedOutStack => [
+        MaterialPage(
+          key: const ValueKey("LoginPage"),
+          child: LoginScreen(
+            onLogin: () {
+              isLoggedIn = true;
+              notifyListeners();
+            },
+            onRegister: () {
+              isRegister = true;
+              notifyListeners();
+            },
+          ),
+        ),
+        if (isRegister == true)
+          MaterialPage(
+            key: const ValueKey("RegisterPage"),
+            child: RegisterScreen(
+              onRegister: () {
+                isRegister = false;
+                notifyListeners();
+              },
+              onLogin: () {
+                isLoggedIn = true;
+                notifyListeners();
+              },
+            ),
+          ),
+      ];
+
+  List<Page> get _loggedInStack => [
+        MaterialPage(
+          key: const ValueKey("QuotesListPage"),
+          child: QuotesListScreen(
+            quotes: quotes,
+            onTapped: (String quoteId) {
+              selectedQuote = quoteId;
+              notifyListeners();
+            },
+            onLogout: () {
+              isLoggedIn = false;
+              notifyListeners();
+            },
+            toFormScreen: () {
+              isForm = true;
+              notifyListeners();
+            },
+          ),
+        ),
+        if (selectedQuote != null)
+          MaterialPage(
+            key: ValueKey(selectedQuote),
+            child: QuoteDetailsScreen(
+              quoteId: selectedQuote!,
+            ),
+          ),
+        if (isForm == true)
+          MaterialPage(
+            key: const ValueKey("FormPage"),
+            child: FormScreen(
+              onSend: () {
+                isForm = false;
+                notifyListeners();
+              },
+            ),
+          )
+      ];
 }
